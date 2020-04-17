8dff7a1c998a5cc630510d5aacc9c4e7f997325f
Stheffany Hadlich <stheffany@codeminer42.com>
2020-04-17 11:14:47 -0300

    Tests responsive breakpoint class

--- test/src/breakpoint_test.dart
+++ test/src/breakpoint_test.dart
@@ -1,47 +1,157 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import '../../lib/src/breakpoint.dart';
+import 'package:responsive_builder/src/breakpoint.dart';
 
 void main() {
   group('ResponsiveBreakpoint', () {
-    group('Factories', () {
-      test('it should define a breakpoint using minimun heigth', () {
-        final minHeight = 100;
-        final result = ResponsiveBreakpoint.byMinHeight(minHeight: minHeight);
+    group('given minHeight', () {
+      test('when minHeight fits its current screen size', () {
+        final minHeight = 1;
+        final size = Size(10, 15);
+        final responsiveBreakpoint = ResponsiveBreakpoint.byMinHeight(minHeight);
 
-        expect(result.minHeight, minHeight);
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
+
+        expect(subject, isTrue);
       });
 
-      test('it should define a breakpoint using minimun width', () {
-        final minWidth = 100;
-        final result = ResponsiveBreakpoint.byMinWidth(minWidth: minWidth);
+      test('when minHeight does not fits its current screen size', () {
+        final minHeight = 20;
+        final size = Size(10, 15);
+        final responsiveBreakpoint = ResponsiveBreakpoint.byMinHeight(minHeight);
 
-        expect(result.minWidth, minWidth);
-      });
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
 
-      test('it should define a breakpoint using maximum heigth', () {
-        final maxHeight = 100;
-        final result = ResponsiveBreakpoint.byMaxHeight(maxHeight: maxHeight);
-
-        expect(result.maxHeight, maxHeight);
-      });
-
-      test('it should define a breakpoint using maximum width', () {
-        final maxWidth = 100;
-        final result = ResponsiveBreakpoint.byMaxWidth(maxWidth: maxWidth);
-
-        expect(result.maxWidth, maxWidth);
-      });
-
-      test('it should define a breakpoint using portrait orientation', () {
-        final result = ResponsiveBreakpoint.byPortraitMode();
-        expect(result.orientation, Orientation.portrait);
-      });
-
-      test('it should define a breakpoint using landscape orientation', () {
-        final result = ResponsiveBreakpoint.byLandscapeMode();
-        expect(result.orientation, Orientation.landscape);
+        expect(subject, isFalse);
       });
     });
+
+    group('given minWidth', () {
+      test('when minWidth fits its current screen size', () {
+        final minWidth = 1;
+        final size = Size(10, 15);
+        final responsiveBreakpoint = ResponsiveBreakpoint.byMinWidth(minWidth);
+
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
+
+        expect(subject, isTrue);
+      });
+
+      test('when minWidth does not fits its current screen size', () {
+        final minWidth = 20;
+        final size = Size(10, 15);
+        final responsiveBreakpoint = ResponsiveBreakpoint.byMinWidth(minWidth);
+
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
+
+        expect(subject, isFalse);
+      });
+    });
+
+    group('given maxHeight', () {
+       test('when maxHeight fits its current screen size', () {
+        final maxHeight = 16;
+        final responsiveBreakpoint = ResponsiveBreakpoint.byMaxHeight(maxHeight);
+        final size = Size(10, 15);
+
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
+
+        expect(subject, isTrue);
+      });
+
+      test('when maxHeight does not fits its current screen size', () {
+        final maxHeight = 14;
+        final size = Size(10, 15);
+        final responsiveBreakpoint = ResponsiveBreakpoint.byMaxHeight(maxHeight);
+
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
+
+        expect(subject, isFalse);
+      });
+    });
+
+    group('given maxWidth', () {
+      test('when maxWidth fits its current screen size', () {
+        final maxWidth = 11;
+        final size = Size(10, 15);
+        final responsiveBreakpoint = ResponsiveBreakpoint.byMaxWidth(maxWidth);
+
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
+
+        expect(subject, isTrue);
+      });
+
+      test('when maxWidth fits its current screen size', () {
+        final maxWidth = 9;
+        final size = Size(10, 15);
+        final responsiveBreakpoint = ResponsiveBreakpoint.byMaxWidth(maxWidth);
+
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
+
+        expect(subject, isFalse);
+      });
+    });
+
+    group('given portrait orientation', () {
+      test('when orientation matches screen orientation', () {
+        final size = Size(10, 15);
+        final responsiveBreakpoint = ResponsiveBreakpoint.byPortraitMode();
+
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.portrait);
+
+        expect(subject, isTrue);
+      });
+
+       test('when orientation does not matches screen orientation', () {
+        final size = Size(10, 15);
+        final responsiveBreakpoint = ResponsiveBreakpoint.byPortraitMode();
+        
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
+
+        expect(subject, isFalse);
+      });
+    });
+
+    group('given landscape orientation', () {
+      test('when orientation matches screen orientation', () {
+        final size = Size(10, 15);
+        final responsiveBreakpoint = ResponsiveBreakpoint.byLandscapeMode();
+
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
+
+        expect(subject, isTrue);
+      });
+
+       test('when orientation does not matches screen orientation', () {
+        final size = Size(10, 15);
+        final responsiveBreakpoint = ResponsiveBreakpoint.byLandscapeMode();
+        
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.portrait);
+
+        expect(subject, isFalse);
+      });
+    });
+
+     group('given maxHeight, maxWidth, MinHeight, minWidth and orientation', () {
+      test('when all params match screen size and orientation', () {
+        final size = Size(5, 10);
+        final responsiveBreakpoint =
+          ResponsiveBreakpoint(maxWidth: 10, maxHeight: 10, minHeight: 5, minWidth: 5, orientation: Orientation.landscape );
+
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
+
+        expect(subject, isTrue);
+      });
+
+      test('when all params match screen size and orientation', () {
+        final size = Size(4, 11);
+        final responsiveBreakpoint =
+          ResponsiveBreakpoint(maxWidth: 10, maxHeight: 10, minHeight: 5, minWidth: 5, orientation: Orientation.landscape );
+
+        final subject = responsiveBreakpoint.itMatchesWith(size: size, currentOrientation: Orientation.landscape);
+
+        expect(subject, isFalse);
+      });
+     });
   });
 }
